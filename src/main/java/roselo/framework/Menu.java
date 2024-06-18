d57ccf8efd2765c98f5b711d2f1af2cf4173a7b0
BenjaminRo01 <benjaroselo01@gmail.com>
2024-06-18 13:04:20 -0300

    Version 1.1 - Se añadio ConsoleUI para la interfaz

--- src/main/java/roselo/framework/Menu.java
+++ src/main/java/roselo/framework/Menu.java
@@ -1,15 +1,26 @@
 package roselo.framework;
 
+import de.codeshelf.consoleui.prompt.ConsolePrompt;
+import de.codeshelf.consoleui.prompt.ListResult;
+import de.codeshelf.consoleui.prompt.PromtResultItemIF;
+import de.codeshelf.consoleui.prompt.builder.ListPromptBuilder;
+import de.codeshelf.consoleui.prompt.builder.PromptBuilder;
+import org.fusesource.jansi.AnsiConsole;
+
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Scanner;
+import java.util.*;
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class Menu {
     private List<Accion> acciones;
     private static final String CONFIG_PROPERTIES_DEFAULT = "/config.properties";
     private static final String CLASS_NAME_PROPERTY = "clases";
+    private ListPromptBuilder listPromptBuilder;
+    private ConsolePrompt prompt;
+    private PromptBuilder promptBuilder;
+
     public Menu(){
         this(CONFIG_PROPERTIES_DEFAULT);
     }
@@ -22,10 +33,12 @@ public class Menu {
         try (InputStream configFile = getClass().getResourceAsStream(path);) {
             prop.load(configFile);
             String[] clases = prop.getProperty(CLASS_NAME_PROPERTY).split(",");
-            for (var c : clases) {
+            for (String c : clases) {
                 var clazz = Class.forName(c);
-                Accion accion = (Accion) clazz.getDeclaredConstructor().newInstance();
-                this.acciones.add(accion);
+                if (Accion.class.isAssignableFrom(clazz)) {
+                    Accion accion = (Accion) clazz.getDeclaredConstructor().newInstance();
+                    this.acciones.add(accion);
+                }
             }
         } catch (Exception e) {
             throw new RuntimeException(
@@ -33,34 +46,40 @@ public class Menu {
         }
     }
     private void mostrarMenu(){
+        AnsiConsole.systemInstall();
+        prompt = new ConsolePrompt();
+        promptBuilder = prompt.getPromptBuilder();
+
+        System.out.println(ansi().eraseScreen().render("@|green ----Menu de acciones---- |@"));
+        listPromptBuilder = promptBuilder.createListPrompt();
+        listPromptBuilder.name("opcion").message("Selecciona una opcion");
+
+        int opcion = 0;
         int num = 1;
-        System.out.println("----Menú de acciones----");
         for(Accion a : this.acciones){
-            System.out.println(num + ". " + a.nombreItemMenu() + "(" + a.descripcionItemMenu() + ")");
+            listPromptBuilder.newItem(String.valueOf(num))
+                    .text(num + ". " + a.nombreItemMenu() + "(" + a.descripcionItemMenu() + ")").add();
             num++;
         }
-        System.out.println("0. Salir");
-        System.out.println("Ingrese su opcion: ");
+        listPromptBuilder.newItem("0").text("Salir").add();
+        listPromptBuilder.addPrompt();
     }
     public void procesarSolicitud(){
-        Scanner scanner = new Scanner(System.in);
-        int opcion = 1;
-        int tamanioList = this.acciones.size();
-
         this.mostrarMenu();
-        opcion = scanner.nextInt();
-        while(opcion != 0){
-            if((opcion > 0) && (opcion <= tamanioList)) {
+        try {
+            HashMap<String, ? extends PromtResultItemIF> result = prompt.prompt(promptBuilder.build());
+            ListResult resultado = (ListResult) result.get("opcion");
+            int opcion = Integer.parseInt(resultado.getSelectedId());
+            while(opcion != 0){
                 this.acciones.get(opcion - 1).ejecutar();
+                result = prompt.prompt(promptBuilder.build());
+                resultado = (ListResult) result.get("opcion");
+                opcion = Integer.parseInt(resultado.getSelectedId());
             }
-            else{
-                System.out.println("Error: selecciona una de las opciones mostradas!");
-            }
-            this.mostrarMenu();
-            opcion = scanner.nextInt();
+            System.out.println("Finalizando ejecucion.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
         //Ver como implementar la notificación de éxito o fracaso de la ejecución.
-        System.out.println("Fin de la ejecución.");
     }
 }
